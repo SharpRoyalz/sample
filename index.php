40837a1faf21ed1fa954ffb6d04dbfb85ec09f35
SharpRoyalz <116153953+SharpRoyalz@users.noreply.github.com>
2022-10-19 16:17:12 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form>
+        <h2>Login Page</h2>
+        <label>User Name</label>
+        <input type="text" name="uname" placeholder="User Name">
+
+        <label>User Name</label>
+        <input type="password" name="password" placeholder="password">
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
